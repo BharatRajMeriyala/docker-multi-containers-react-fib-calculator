2b8536fe90186fc33b528a77582805108ab47ccb
Sean Hong <iamseanhong@gmail.com>
2018-09-12 21:17:28 -0700

    Setup server/index.js

--- server/index.js
+++ server/index.js
@@ -31,6 +31,43 @@ pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch(err => {
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000
+  retry_strategy: () => 1000,
 });
 const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+app.get('/', (req, res) => {
+  res.send('Working?');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+app.get('values/current', (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.postMessage('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    // Takes too long time to calculate if it is more than 40
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+const PORT = 5000;
+app.listen(PORT, err => {
+  console.log(`Listening to PORT:${PORT}`);
+});
