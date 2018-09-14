5c2126c6707d0cf0ed56f5b1cd037257df6b784d
Sean Hong <iamseanhong@gmail.com>
2018-09-13 20:55:41 -0700

    Fix bug related to router

---
+++ client/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//
+import logo from './logo.svg';
+import './App.css';
+
+//
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+      <Link to="/"> Home </Link>
+      <Link to="/other-page"> OTHER PAGE </Link>
+    </header>
+  );
+};
+
+export default Header;
