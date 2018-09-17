1c65415df6f7f3783962d26427457124e8014af5
Sean Hong <iamseanhong@gmail.com>
2018-09-16 19:42:33 -0700

    Change header

--- client/src/Header.js
+++ client/src/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">Changed ~~~</h1>
       <Link to="/"> Home </Link>
       <Link to="/other-page"> OTHER PAGE </Link>
     </header>
