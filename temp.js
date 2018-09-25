4942e7dedb0f72db3fc2b7a27e15ccb5df60eecc
Sean Hong <iamseanhong@gmail.com>
2018-09-24 20:53:33 -0700

    Remove ':' ah... so hard to find this bug ...

---
+++ temp.js
@@ -0,0 +1,19 @@
+// server {
+//   listen 3000;
+
+//   location / {
+//     root /usr/share/nginx/html;
+//     index index.html index.htm;
+//   }
+// }
+
+// !!!!!!!!!!!!!!!! AH !!!!!!!!!!!!!!!!!!!!!!!!!
+// server {
+//   listen: 3000;
+
+//   location / {
+//     root /usr/share/nginx/html;
+//     index index.html index.htm;
+//     try_files $uri $uri/ /index.html;
+//   }
+// }
